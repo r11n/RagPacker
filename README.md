442185760ca7e39d7f57274c001b516c778d4de0
r11n <nekkanti.raghavendra2@gmail.com>
2021-01-04 14:01:43 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# RagPacker
+A readymade solution for integrating webpack into django based applications.
---
+++ setup.py
@@ -0,0 +1,15 @@
+from distutils.core import setup
+setup(
+    name='RagPacker',
+    packages=['ragpacker'],
+    version='0.0.1',
+    license='MIT',
+    description='A readymade solution for integrating webpack into django based applications.',
+    author='r11n',
+    author_email='nekkanti.raghavendra2@gmail.com',
+    url='https://github.com/r11n/ragpacker',
+    download_url='https://github.com/r11n/ragpacker/archive/0.0.1.tar.gz',
+    keywords=['webpack', 'django-webpack'],
+    # TODO: add install requires
+    # TODO: add classifiers
+)
